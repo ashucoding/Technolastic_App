bc2160e704fa6fb096fa97a553478a99438a9cce
ashucoding <shanawiltshire@gmail.com>
2021-01-31 20:43:48 -0500

    start btn working

---
+++ js/script.js
@@ -0,0 +1,15 @@
+// required elememts//
+const start_btn = document.querySelector(".start_btn button");
+const info_box = document.querySelector(".info_box");
+const exit_btn = info_box.querySelector(".buttons .quit");
+const continue_btn = info_box.querySelector(".buttons .restart");
+
+
+//start quiz button//
+start_btn.onclick = ()=>{
+    info_box.classList.add("activeInfo");
+}
+
+start_btn.onclick = ()=>{
+    info_box.classList.add("activeInfo");
+}
