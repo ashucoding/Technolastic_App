5e6f9b51e3b57ef334f0dbc52969deccc85ad2fd
ashucoding <shanawiltshire@gmail.com>
2021-02-01 00:05:37 -0500

    adding questions in array

---
+++ js/questions.js
@@ -0,0 +1,26 @@
+let questions = [
+    { 
+        numb: 1,
+        question: "what does DOM stand for?",
+        answer: "Document Object Model",
+        options: [
+            "Dictators Objecting Madness",
+            "Document Object Model",
+            "Drinks On Me",
+            "Digital Output Module"
+        ]
+    },
+   
+{ 
+    numb: 2,
+    question: "what does CSS stand for?",
+    answer: "Cascading Style Sheet",
+    options: [
+        "Common Style Sheet",
+        "Colorful Style Sheet",
+        "Computer Style Sheet",
+        "Cascading Style Sheet"
+    ]
+};
+];
+
