0ac791cd79d0ab4912824391bbc03d6f47f4592b
ashucoding <shanawiltshire@gmail.com>
2021-02-02 14:55:28 -0500

    question number que working

--- js/questions.js
+++ js/questions.js
@@ -1,7 +1,7 @@
 let questions = [
     { 
         numb: 1,
-        question: "what does DOM stand for?",
+        question: "What does DOM stand for?",
         answer: "Document Object Model",
         options: [
             "Dictators Objecting Madness",
@@ -13,14 +13,97 @@ let questions = [
    
 { 
     numb: 2,
-    question: "what does CSS stand for?",
-    answer: "Cascading Style Sheet",
+    question: "What are the different data types present in javascript?",
+    answer: "Primitive and Non Primitive",
     options: [
-        "Common Style Sheet",
-        "Colorful Style Sheet",
-        "Computer Style Sheet",
-        "Cascading Style Sheet"
+        "Basic and Non Basic",
+        "Primitive and Non Primitive",
+        "Real and Fake",
+        "True and False"
     ]
-};
+},
+{ 
+    numb: 3,
+    question: "Explain passed by value and passed by reference",
+    answer: "Primitive data types are passed by value and non-primitive data types are passed by reference",
+    options: [
+        "Non-primitive data types are passed by value and primitive data types are passed by reference",
+        "True data types are passed by value and false data types are passed by reference",
+        "Primitive data types are passed by value and non-primitive data types are passed by reference",
+        "Boolean values are only passed by the value and all other data are passed by reference"
+    ]
+},
+{    numb: 4,
+    question: "Which statement is true about 'this' keyword",
+    answer: "The value of “this” keyword will always depend on the object that is invoking the function",
+    options: [
+        "The value of “this” keyword will always depend on the object that is invoking the function",
+        "'This' keyword will always rename the object",
+        "'This' keyword replaces 'const'",
+        "'This' is a not a reseverd identifier"
+    ]
+ },
+
+ {    numb: 5,
+    question: "What are three types of scopes in JS?",
+    answer: "Global, Local, Block",
+    options: [
+        "False, True, and Undefined",
+        "World, State, City",
+        "Global, Local, Block",
+        "Tom, Dick, and Harry"
+    ]
+ },
+
+ {    numb: 6,
+    question: "What statement best describes Closures in JS?",
+    answer: "Closures is an ability of a function to remember the variables and functions that are declared in its outer scope",
+    options: [
+        "The end of your program",
+        "Closures is an ability of a function to remember the variables and functions that are declared in its outer scope",
+        "The end of a line of code",
+        "Describe the variables in a scope"
+    ]
+ },
+ {    numb: 7,
+    question: "What are object prototypes?",
+    answer: "A prototype is a blueprint of an object",
+    options: [
+        "A demo of your app",
+        "A prototype is a blueprint of an object",
+        "A physical model of objects in js",
+        "Programming structure"
+    ]
+ },
+ {    numb: 8,
+    question: "What are callbacks?",
+    answer: "Functions that are used as an argument to another function",
+    options: [
+        "Functions that are used as an argument to another function",
+        "Copy and Paste of a function",
+        "An argument of a function",
+        "Multiple arguments of a function."
+    ]
+ },
+ {    numb: 9,
+    question: "What is recursion in a programming language?",
+    answer: "A technique to iterate over an operation by having a function call itself repeatedly until it arrives at a result",
+    options: [
+        "Another word for iterate",
+        "Characterized by recurrence or repetition",
+        "To destroy your code",
+        "A technique to iterate over an operation by having a function call itself repeatedly until it arrives at a result"
+    ]
+ },
+ {    numb: 10,
+    question: "What is the use of promises in javascript?",
+    answer: "Promises are used to handle asynchronous operations in javascript",
+    options: [
+        "A declaration or assurance that one will do a particular thing or that a particular thing will happen",
+        "Read ME declaration of love for your program",
+        "Promises are used to handle asynchronous operations in javascript",
+        "A technique to iterate over an operation by having a function call itself repeatedly until it arrives at a result"
+    ]
+ }
 ];
 
